167fd8abf564081322a4ab79b4ab74c2a1b94ef2
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2023-02-05 10:55:43 +0100

    - DivisibilityCheckerTest with ValueSource

--- src/test/java/com/sda/testingadvanced/solution/parametrized/DivisibilityCheckerTest.java
+++ src/test/java/com/sda/testingadvanced/solution/parametrized/DivisibilityCheckerTest.java
@@ -1,13 +1,24 @@
 package com.sda.testingadvanced.solution.parametrized;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import com.sda.testingadvanced.parametrized.DivisibilityChecker;
 
 class DivisibilityCheckerTest {
 
 	@ParameterizedTest
-	@ValueSource
+	@ValueSource(ints = {-3, 0, 3, 9, 12})
 	void shouldBeDivisibleBy3(Integer number) {
-		//assertTrue(result);
+		assertTrue(DivisibilityChecker.isDivisibleBy3(number));
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = {-2, 1, 4})
+	void shouldNotBeDivisibleBy3(Integer number) {
+		assertFalse(DivisibilityChecker.isDivisibleBy3(number));
 	}
 }
