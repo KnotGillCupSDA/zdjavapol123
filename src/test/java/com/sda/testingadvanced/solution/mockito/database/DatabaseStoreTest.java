0c62c4260b077dce77ebecd4063818574cca067a
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2023-02-05 15:54:29 +0100

    - DatabaseStoreTest draft

---
+++ src/test/java/com/sda/testingadvanced/solution/mockito/database/DatabaseStoreTest.java
@@ -0,0 +1,20 @@
+package com.sda.testingadvanced.solution.mockito.database;
+
+import org.junit.jupiter.api.Test;
+
+//TODO: Could extend with MockitoExtension
+public class DatabaseStoreTest {
+
+	//TODO: create DatabaseConnection mock
+
+	//TODO: inject mocks into DatabaseStore
+
+	@Test
+	void shouldAddDataForOpenedDatabaseConnection() {
+		//TODO: model that connection is already opened (databaseConnection.isOpened)
+
+		//TODO: Call databaseStore.addData
+
+		//TODO:add verifies/asserts
+	}
+}
--- src/test/java/com/sda/testingadvanced/solution/mockito/user/UserServiceTest.java
+++ src/test/java/com/sda/testingadvanced/solution/mockito/user/UserServiceTest.java
@@ -1,4 +1,4 @@
-package com.sda.testingadvanced.solution.mockito;
+package com.sda.testingadvanced.solution.mockito.user;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
