9703610f3e10c6ba5441f955a31154eb99a5d633
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2023-02-05 11:16:17 +0100

    - CsvSource introduced

--- src/test/java/com/sda/testingadvanced/solution/parametrized/DivisibilityCheckerTest.java
+++ src/test/java/com/sda/testingadvanced/solution/parametrized/DivisibilityCheckerTest.java
@@ -1,9 +1,11 @@
 package com.sda.testingadvanced.solution.parametrized;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -23,4 +25,13 @@ class DivisibilityCheckerTest {
 	void shouldNotBeDivisibleBy3(Integer number) {
 		assertFalse(DivisibilityChecker.isDivisibleBy3(number));
 	}
+
+
+	@ParameterizedTest
+	@CsvSource(value = {"-2, false", "-3, true", "0, true", "1, false", "3, true"})
+	void testDivisibilityBy3(Integer number, boolean expected) {
+		boolean result = DivisibilityChecker.isDivisibleBy3(number);
+		assertEquals(expected, result);
+	}
+
 }
