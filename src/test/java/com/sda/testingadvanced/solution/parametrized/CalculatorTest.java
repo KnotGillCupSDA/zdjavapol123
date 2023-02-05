e7cfcf2595e6c2de2125abd4d65e2cf94a6adb1e
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2023-02-05 09:46:01 +0100

    - CalculatorTest annotation test

--- src/test/java/com/sda/testingadvanced/solution/parametrized/CalculatorTest.java
+++ src/test/java/com/sda/testingadvanced/solution/parametrized/CalculatorTest.java
@@ -1,5 +1,40 @@
 package com.sda.testingadvanced.solution.parametrized;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class CalculatorTest {
 
+	@BeforeAll
+	static void beforeAll() {
+		System.out.println("beforeAll");
+	}
+
+	@AfterAll
+	static void afterAll() {
+		System.out.println("afterAll");
+	}
+
+	@BeforeEach
+	void setUp() {
+		System.out.println("setUp");
+	}
+
+	@AfterEach
+	void tearDown() {
+		System.out.println("tearDown");
+	}
+
+	@Test
+	void shouldAddTwoNegativeNumbers() {
+		System.out.println("shouldAddTwoPositiveNumbers");
+	}
+
+	@Test
+	void shouldAddTwoPositiveNumbers() {
+		System.out.println("shouldAddTwoPositiveNumbers");
+	}
 }
